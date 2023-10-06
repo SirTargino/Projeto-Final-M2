4a44b351228d2d1854648928a56544f894510ad1
João Targino <joaoetargino@gmail.com>
2023-10-06 20:09:09 -0300

    Register screen and fix problems

--- src/models/Person.model.js
+++ src/models/Person.model.js
@@ -1,13 +1,11 @@
-class Person {
-    #password
+class Person{
     constructor(name, age, gender, location, email, password, id) {
         this.name = name;
         this.age = age;
         this.gender = gender;
         this.location = location;
         this.email = email;
-        this.#password = password
+        this.password = password
         this.id = id;
     }
-
 }
